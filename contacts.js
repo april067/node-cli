6f38a5399bc1d937a08000bf6c61f5dabd840d9d
Iker King <ik@ik-2.local>
2024-10-06 20:23:10 +0300

    list- , get- actions

---
+++ contacts.js
@@ -0,0 +1,36 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+async function listContacts() {
+	const data = await fs.readFile(contactsPath);
+	return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+	const contacts = await listContacts();
+	const contact = contacts.find((item) => item.id === contactId);
+	if (!contact) return null;
+	return contact;
+}
+
+async function removeContact(contactId) {
+	const contacts = await listContacts();
+}
+
+async function addContact(name, email, phone) {
+	// ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+async function updateContact(contactId, name, email, phone) {
+	// ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+module.exports = {
+	listContacts,
+	getContactById,
+	removeContact,
+	addContact,
+	updateContact,
+};
